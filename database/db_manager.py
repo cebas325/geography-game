08ed8774d87fe3493410621e3d0bf60c659fc732
cebas <miguel.ceballos00@gmail.com>
2023-06-19 19:16:32 +0200

    create spain schema

--- database/db_manager.py
+++ database/db_manager.py
@@ -3,12 +3,18 @@ import pandas as pd
 import sqlite3
 
 
-conn = sqlite3.connect("data/standings.db")
-pd.read_sql_query("""
-    CREATE TABLE IF NOT EXISTS Spain (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    surname TEXT,
-    score FLOAT
-    )
-""")
+def connect_db():
+    conn = sqlite3.connect("data/standings.db")
+    return conn.cursor()
+
+
+def create_spain_table():
+    cur = connect_db()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Spain (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            surname TEXT,
+            score FLOAT
+        )
+    """)
